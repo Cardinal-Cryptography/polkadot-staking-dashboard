2b73df47e15f80bf32b595d12cfe09ff9859cbea
Piotr Dżaluk <piotr.dzaluk@cardinals.cc>
2023-03-16 10:25:12 +0100

    A0-2066: Remove Subscan remainings

--- src/library/Graphs/types.ts
+++ src/library/Graphs/types.ts
@@ -1,19 +1,12 @@
 // Copyright 2022 @paritytech/polkadot-staking-dashboard authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { AnySubscan } from 'types';
-
 export interface BondedProps {
   active: number;
   unlocking: number;
   unlocked: number;
   inactive: boolean;
   free: number;
-}
-
-export interface EraPointsProps {
-  items: AnySubscan;
-  height: number;
 }
 
 export interface PayoutBarProps {
